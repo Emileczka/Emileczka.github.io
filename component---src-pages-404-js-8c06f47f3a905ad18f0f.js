a535e780d41aa6de010ae14d76498baecd66b865
Your Name <flisikowska.emilia@gmail.com>
2021-01-15 16:56:04 +0100

    First commit- Portfolio

---
+++ component---src-pages-404-js-8c06f47f3a905ad18f0f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,t,e){"use strict";e.r(t);var o=e("q1tI"),a=e.n(o),r=e("vrFN");t.default=function(){return a.a.createElement(r.a,{title:"404: Not found"})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8c06f47f3a905ad18f0f.js.map
